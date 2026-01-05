abc1bfca7c98813cdb6ef8e478cfcff048ab2029
Shubham Goel <shubhamgoel27@gmail.com>
2026-01-04 23:50:02 -0800

    fix: make pysqlite3 import optional for local development

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
 
 import streamlit as st
 import utils
